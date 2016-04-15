0ed19b41d70fb45b99809499554fc14a64ef4c36
Asanilta Fahda <asanilta@gmail.com>
2016-04-15 17:43:29 +0700

    gabung dgn backend

--- controllers/tenderController.js
+++ controllers/tenderController.js
@@ -1,24 +1,32 @@
 tesaApp.controller('tenderController',['$scope','$http', function($scope, $http) {
   var API = 'codeigniter/api/tender/';
-  $http.get(API + 'get/all').success(function(data){
-        $scope.tenders = data.content;
-    }).error(function(data){
-        alert("Error");
-    });
+  var USER_API = 'codeigniter/api/user/';
 
-    $scope.selectTender = function(index) {
-      $scope.selectedTender = $scope.tenders[index];
+  $http.get(USER_API + 'get/asanilta').success(function(data){
+    $scope.user = data.content;
+    $scope.tenders = $scope.user.tender;
+  });
+
+  $http.get(USER_API + 'get/all').success(function(data){
+    $scope.tims = data.content;
+  });
+
+    $scope.selectTender = function(id) {
+      $http.get(API + 'get/' + id).success(function(data){
+            $scope.selectedTender = data.content;
+        }).error(function(data){
+            alert("Error");
+        });
       $('#pilih-tender').modal('hide');
     }
 
     $scope.bidangs = ["IT","Agrikultur","Tata Kota"];
-    $scope.tims = [{"username":"asanilta","nama_lengkap":"Asanilta Fahda"},{"username":"vanya","nama_lengkap":"Vanya Deasy Safrina"}];
 
-    $scope.selected_perusahaan = {"nama_perusahaan": "PT ABC","alamat":"Jalan ABC No. 123","telp":"022 7272727","email":"abc@abc.com","bidang":["IT", "Tata Kota"]};
-    $scope.rekomendasi_perusahaan = {"nama_perusahaan": "PT DEF","alamat":"Jalan DEF No. 123","telp":"022 27272727","email":"def@def.com","bidang":["IT"]};
-    $scope.rekomendasi_tenaga_ahli = {"id_ktp":"12345","nama_tenaga_ahli":"Asanilta Fahda","jenis_kelamin":"P","telp":"081219705514","bidang_keahlian":["IT"]};
-    $scope.selected_tenaga_ahli = [{"nama_tenaga_ahli":"Vanya Deasy Safrina","jenis_kelamin":"P","telp":"08121111111","bidang_keahlian":["IT"]},{"nama_tenaga_ahli":"Asep","jenis_kelamin":"L","telp":"0812222222","bidang_keahlian":["Agrikultur"]}];
-    $scope.rekomendasi_tender = [{"nama_tender":"Pekerjaan Blablabla","instansi_pengada":"Kemeterian Perdamaian","tim":["Asanilta Fahda","Vanya Deasy Safrina"],"bidang":["IT"]}];
+    // $scope.selected_perusahaan = {"nama_perusahaan": "PT ABC","alamat":"Jalan ABC No. 123","telp":"022 7272727","email":"abc@abc.com","bidang":["IT", "Tata Kota"]};
+    // $scope.rekomendasi_perusahaan = {"nama_perusahaan": "PT DEF","alamat":"Jalan DEF No. 123","telp":"022 27272727","email":"def@def.com","bidang":["IT"]};
+    // $scope.rekomendasi_tenaga_ahli = {"id_ktp":"12345","nama_tenaga_ahli":"Asanilta Fahda","jenis_kelamin":"P","telp":"081219705514","bidang_keahlian":["IT"]};
+    // $scope.selected_tenaga_ahli = [{"nama_tenaga_ahli":"Vanya Deasy Safrina","jenis_kelamin":"P","telp":"08121111111","bidang_keahlian":["IT"]},{"nama_tenaga_ahli":"Asep","jenis_kelamin":"L","telp":"0812222222","bidang_keahlian":["Agrikultur"]}];
+    // $scope.rekomendasi_tender = [{"nama_tender":"Pekerjaan Blablabla","instansi_pengada":"Kemeterian Perdamaian","tim":["Asanilta Fahda","Vanya Deasy Safrina"],"bidang":["IT"]}];
 
     $scope.toggle = function(modalstate) {
       // $scope.dataForm.$setUntouched();
